e6ca73416da0b544374d3a726365014f4bd2c06f
unknown <mkaiser101@gmail.com>
2018-11-27 19:18:42 -0700

    added base with some paralax

---
+++ src/components/home.vue
@@ -0,0 +1,12 @@
+<template>
+
+<v-parallax :speed-factor="0.3">
+  <img src="../assets/flat.jpg" alt="very cool bg">
+
+</v-parallax>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
