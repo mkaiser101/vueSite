cd12400a3de69596818b8e04d78e49ee46cff534
unknown <mkaiser101@gmail.com>
2018-12-11 18:22:52 -0700

    new code

--- src/App.vue
+++ src/App.vue
@@ -19,26 +19,8 @@
       light
       absolute
     >
-      <v-toolbar flat class="transparent">
-      <v-list class="pa-0">
-        <v-list-tile avatar>
-          <v-list-tile-avatar>
-            <img src="https://randomuser.me/api/portraits/men/85.jpg" />
-          </v-list-tile-avatar>
-          <v-list-tile-content>
-            <v-list-tile-title>John Leider</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-toolbar>
-    <v-list class="pt-0" dense>
-      <v-divider></v-divider>
-      <v-list-tile>
-        <v-list-tile-action>
-          <v-icon></v-icon>
-        </v-list-tile-action>
-      </v-list-tile>
-    </v-list>
+
+    
     </v-navigation-drawer>
   </v-app>
 </template>
--- src/components/home.vue
+++ src/components/home.vue
@@ -1,12 +1,80 @@
+
 <template>
 
-<v-parallax :speed-factor="0.3">
-  <img src="../assets/flat.jpg" alt="very cool bg">
 
-</v-parallax>
+  <v-layout>
+    <v-flex xs12 sm6 offset-sm3>
+      <v-card>
+        <v-container grid-list-sm fluid>
+          <v-layout row wrap>
+            <v-flex
+              v-for="n in 9"
+              :key="n"
+              xs4
+              d-flex
+            >
+              <v-card flat tile class="d-flex">
+                <v-img
+                  :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+                  :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
+                  aspect-ratio="1"
+                  class="grey lighten-2"
+                >
+                  <v-layout
+                    slot="placeholder"
+                    fill-height
+                    align-center
+                    justify-center
+                    ma-0
+                  >
+                    <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                  </v-layout>
+                </v-img>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-card>
+    </v-flex>
+  </v-layout>
+
+
 </template>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
 
-</style>
+<template>
+
+
+  <v-carousel>
+    <v-carousel-item
+      v-for="(item,i) in items"
+      :key="i"
+      :src="item.src"
+    ></v-carousel-item>
+  </v-carousel>
+
+
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        items: [
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'
+          }
+        ]
+      }
+    }
+  }
+</script>
