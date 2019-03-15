55236702c18e1bb759268fcc2e4d05ebf64c2f25
unknown <mkaiser101@gmail.com>
2019-03-15 16:33:56 -0600

    added grid and parallax, fun stuff

--- src/components/home.vue
+++ src/components/home.vue
@@ -2,6 +2,112 @@
 <template>
   <div>
 
+  <v-layout row>
+     <v-flex xs6 order-lg2>
+       <v-card dark tile flat color="error">
+       <v-carousel>
+         <v-carousel-item
+           v-for="(item,i) in items"
+           :key="i"
+           :src="item.src"
+         ></v-carousel-item>
+       </v-carousel>
+       </v-card>
+     </v-flex>
+     <v-flex xs6>
+       <v-card dark tile flat color="red darken-4">
+       <v-carousel>
+         <v-carousel-item
+           v-for="(item,i) in items"
+           :key="i"
+           :src="item.src"
+         ></v-carousel-item>
+       </v-carousel>
+       </v-card>
+     </v-flex>
+   </v-layout>
+
+  <v-layout row>
+       <v-flex
+         grow
+         pa-1
+       >
+         <v-card
+           dark
+           color="green darken-3"
+         >
+         <v-container grid-list-sm fluid>
+           <v-layout row wrap>
+             <v-flex
+               v-for="n in 9"
+               :key="n"
+               xs4
+               d-flex
+             >
+               <v-card flat tile class="d-flex">
+                 <v-img
+                   :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+                   :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
+                   aspect-ratio="1"
+                   class="grey lighten-2"
+                 >
+                   <v-layout
+                     slot="placeholder"
+                     fill-height
+                     align-center
+                     justify-center
+                     ma-0
+                   >
+                     <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                   </v-layout>
+                 </v-img>
+               </v-card>
+             </v-flex>
+           </v-layout>
+         </v-container>
+         </v-card>
+       </v-flex>
+       <v-flex
+         shrink
+         pa-1
+       >
+         <v-card
+           dark
+           color="green darken-1"
+         >
+         <v-container grid-list-sm fluid>
+           <v-layout row wrap>
+             <v-flex
+               v-for="n in 9"
+               :key="n"
+               xs4
+               d-flex
+             >
+               <v-card flat tile class="d-flex">
+                 <v-img
+                   :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+                   :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
+                   aspect-ratio="1"
+                   class="grey lighten-2"
+                 >
+                   <v-layout
+                     slot="placeholder"
+                     fill-height
+                     align-center
+                     justify-center
+                     ma-0
+                   >
+                     <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                   </v-layout>
+                 </v-img>
+               </v-card>
+             </v-flex>
+           </v-layout>
+         </v-container>
+         </v-card>
+       </v-flex>
+     </v-layout>
+
   <v-layout>
     <v-flex xs12 sm6 offset-sm3>
       <v-card>
--- src/components/second.vue
+++ src/components/second.vue
@@ -1,59 +1,19 @@
-
 <template>
-  <div>
-
-  <v-layout>
-    <v-flex xs12 sm6 offset-sm3>
-      <v-card>
-        <v-container grid-list-sm fluid>
-          <v-layout row wrap>
-            <v-flex
-              v-for="n in 9"
-              :key="n"
-              xs4
-              d-flex
-            >
-              <v-card flat tile class="d-flex">
-                <v-img
-                  :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
-                  :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
-                  aspect-ratio="1"
-                  class="grey lighten-2"
-                >
-                  <v-layout
-                    slot="placeholder"
-                    fill-height
-                    align-center
-                    justify-center
-                    ma-0
-                  >
-                    <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
-                  </v-layout>
-                </v-img>
-              </v-card>
-            </v-flex>
-          </v-layout>
-        </v-container>
-      </v-card>
-    </v-flex>
-
-  </v-layout>
-
-
-    <v-carousel>
-      <v-carousel-item
-        v-for="(item,i) in items"
-        :key="i"
-        :src="item.src"
-      ></v-carousel-item>
-    </v-carousel>
-
-
-
-
-
-
-</div>
-
-
+<div>
+  <v-parallax
+    dark
+    src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+  >
+    <v-layout
+      align-center
+      column
+      justify-center
+    >
+      <h1 class="display-2 font-weight-thin mb-3">Vuetify.js</h1>
+      <h4 class="subheading">Build your application today!</h4>
+    </v-layout>
+  </v-parallax>
+  
+  <v-parallax src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"></v-parallax>
+  </div>
 </template>
