7f710a7930249c50436b78083d12b62bb099e02d
unknown <mkaiser101@gmail.com>
2019-05-25 12:21:31 -0600

    adding script and template logic for reusable components

--- src/components/timeline.vue
+++ src/components/timeline.vue
@@ -6,7 +6,7 @@
         :key="i"
         :color="item.color"
         :icon="item.icon"
-        fill-dot
+        large
       >
         <v-card
           :color="item.color"
@@ -19,6 +19,7 @@
             <v-card-text>
              {{ item.text }}
           </v-card-text>
+           <v-btn round :color="item.color" :to="item.route">{{ item.buttonText }}</v-btn>
         </v-card>
       </v-timeline-item>
     </v-timeline>
@@ -32,30 +33,47 @@
           {
             color: 'red lighten-2',
             icon: 'mdi-briefcase',
-            text: 'hello',
+            text: 'Deploy software updates and patches to servers, write python scripts for automation, and test functionality of new open source solutions within Visas global network.',
             title: 'Associate Cyber Secuirty Engineer',
-            header: '2018 - Present --- Visa Inc'
+            header: '2018 - Present --- Visa Inc --- Ashburn, VA',
+            buttonText: 'about',
+            route: '/about'
+          },
+          {
+            color: 'blue lighten-2',
+            icon: 'mdi-briefcase',
+            text: 'Helping students learn course content in core Computer Science, Physics, and Calculus classes at the University of Colorado.  Went to lecture halls pitching the company in its easrly stages in frnt of lecture halls and classrooms getting students signed up and informed about the service.',
+            title: 'Teacher, Tutor, and Spokesperson',
+            header: '2017 - 2018 --- Penji --- Boulder, CO',
+            buttonText: 'home',
+            route: '/home'
           },
           {
             color: 'purple darken-1',
-            icon: 'mdi-book-variant',
-            text: 'hello',
-            title: 'Associate Cyber Secuirty Engineer',
-            header: 'Visa Inc'
+            icon: 'mdi-briefcase',
+            text: 'Computer Systems (CSCI 2400) Course Assistant for three semesters. I helped students prepare for grading interviews, write c and x86 assembly code, and learn the ins and outs of gdb, the GNU debugger. Collaborated with the professor and teaching assistants to effectively communicate key concepts such as memory management, hardware architecture, and stack visualization.',
+            title: 'Computer Science Course Assistant',
+            header: '2017 - 2018 --- University of Colorado at Boulder',
+            buttonText: 'click me',
+            route: '/about'
           },
           {
             color: 'green lighten-1',
             icon: 'mdi-airballoon',
             text: 'hello',
             title: 'Associate Cyber Secuirty Engineer',
-            header: 'Visa Inc'
+            header: 'Visa Inc',
+            buttonText: 'click me',
+            route: '/about'
           },
           {
             color: 'indigo',
             icon: 'mdi-buffer',
             text: 'hello',
             title: 'Associate Cyber Secuirty Engineer',
-            header: 'Visa Inc'
+            header: 'Visa Inc',
+            buttonText: 'click me',
+            route: '/about'
           }
         ]
       }
